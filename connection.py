1cb585d26220e566c977099cf3e5cba104c51e7c
fleshmanj <fleshmanjoshuac@hotmail.com>
2022-09-12 09:31:53 -0500

    testing

--- connection.py
+++ connection.py
@@ -25,7 +25,7 @@ class Sconnection(Connection):
 
     def __init__(self):
         super().__init__()
-        self.connections_out = {}
+        self.connections_out = []
         self.sock_in = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock_in.bind((HOSTNAME, PORT))
         self.sock_in.listen()
@@ -33,16 +33,19 @@ class Sconnection(Connection):
     def recv(self):
         while True:
             conn, addr = self.sock_in.accept()
-            self.connections_out[addr] = conn
+            # self.connections_out[addr] = conn
+            self.connections_out.append(conn)
             while True:
                 data = conn.recv(4096)
                 if data != b'':
+                    print(f"received data from {addr}")
                     print(data.decode())
 
     def connect(self, address):
         temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         temp.connect(address)
+        self.connections_out.append(temp)
 
     def send(self, message: str):
-        for addr, connection in self.connections_out.items():
+        for connection in self.connections_out:
             connection.send(message.encode())
